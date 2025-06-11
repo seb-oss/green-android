752afcf1a644b25b2c9d1ddb1c436e9b99c93817
Fredrik Eneroth <fredrik.eneroth@seb.se>
2025-06-11 08:22:16 +0200

    Remove Compose BOM and specify versions
    This commit removes the `androidx.compose.bom` from the dependencies.
    Instead, specific versions are now defined for individual Compose libraries in `libs.versions.toml`.
    
    The `compose` version is set to `1.8.2` and `material3` to `1.3.2`.
    The deprecated `appcompat` and `material` libraries have also been removed.

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -49,7 +49,6 @@ mavenPublishing {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
