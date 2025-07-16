2c7f874d7170e2c3be9a30d317d8a0db6316557e
Lisandra Santos <lisandra.santos@seb.se>
2025-07-16 09:05:42 +0200

    Create basic text component

---
+++ components/src/main/kotlin/se/seb/gds/atoms/GdsText.kt
@@ -0,0 +1,23 @@
+package se.seb.gds.atoms
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import se.seb.gds.theme.GdsTheme
+
+@Composable
+fun GdsText(
+    modifier: Modifier = Modifier,
+    label: String,
+    color: Color?,
+    style: TextStyle?
+) {
+    Text(
+        modifier = modifier,
+        text = label,
+        style = style ?: GdsTheme.typography.Body,
+        color = color ?: GdsTheme.colors.contentContent01,
+    )
+}
