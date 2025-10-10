2cde2f0215dcc65ab1cac177653cbde1471d882b
Fredrik Eneroth <fredrik.eneroth@seb.se>
2025-10-10 15:21:36 +0200

    Add some more fonts from Green

--- components/src/main/kotlin/se/seb/gds/theme/GdsTypography.kt
+++ components/src/main/kotlin/se/seb/gds/theme/GdsTypography.kt
@@ -8,43 +8,264 @@ import androidx.compose.ui.unit.sp
  * GDS Typography styles, based on SEB Sans Serif font.
  */
 object GdsTypography {
-    val DetailMediumLarge =
-        TextStyle(
-            fontFamily = GdsSansSerif,
-            fontSize = 16.sp,
-            lineHeight = 20.sp,
-            fontWeight = FontWeight.Medium,
-        )
+    val HeadingXl = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 32.sp,
+        lineHeight = 44.sp,
+        fontWeight = FontWeight.Medium,
+    )
 
-    val DetailBookLarge =
-        TextStyle(
-            fontFamily = GdsSansSerifBook,
-            fontSize = 16.sp,
-            lineHeight = 20.sp,
-            fontWeight = FontWeight.Normal,
-        )
+    val HeadingL = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 28.sp,
+        lineHeight = 40.sp,
+        fontWeight = FontWeight.Medium,
+    )
 
-    val DetailBookMedium =
-        TextStyle(
-            fontFamily = GdsSansSerifBook,
-            fontSize = 14.sp,
-            lineHeight = 16.sp,
-            fontWeight = FontWeight.Normal,
-        )
+    val HeadingM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        fontWeight = FontWeight.Medium,
+    )
 
-    val DetailBookSmall =
-        TextStyle(
-            fontFamily = GdsSansSerifBook,
-            fontSize = 12.sp,
-            lineHeight = 16.sp,
-            fontWeight = FontWeight.Normal,
-        )
+    val HeadingS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        fontWeight = FontWeight.Medium,
+    )
 
-    val DetailRegularSmall =
-        TextStyle(
-            fontFamily = GdsSansSerif,
-            fontSize = 14.sp,
-            lineHeight = 20.sp,
-            fontWeight = FontWeight.Normal,
-        )
+    val HeadingXs = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val Heading2xs = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DetailBookM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DetailBookS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DetailBookXs = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DetailRegularM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val DetailRegularS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val DetailRegularXs = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val BodyBookL = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val BodyBookM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val BodyBookS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val BodyRegularL = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val BodyRegularM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val BodyRegularS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val BodyItalicL = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val BodyItalicM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val BodyItalicS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val Display2xl = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 82.sp,
+        lineHeight = 90.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DisplayXl = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 64.sp,
+        lineHeight = 72.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DisplayL = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 48.sp,
+        lineHeight = 56.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DisplayM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DisplayS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val Preamble2xl = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val PreambleXl = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val PreambleL = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val PreambleM = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val PreambleS = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 18.sp,
+        lineHeight = 26.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val PreambleXs = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    // OLD
+
+    val DetailMediumLarge = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+    )
+
+    val DetailBookLarge = TextStyle(
+        fontFamily = GdsSansSerifBook,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val DetailBookMedium = TextStyle(
+        fontFamily = GdsSansSerifBook,
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val DetailBookSmall = TextStyle(
+        fontFamily = GdsSansSerifBook,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal,
+    )
+
+    val DetailRegularSmall = TextStyle(
+        fontFamily = GdsSansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Normal,
+    )
 }
