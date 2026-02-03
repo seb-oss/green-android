c14355f69533dff7caeeb3917ef1fd059b79527b
Lisandra Santos <lisandra.santos@seb.se>
2026-02-03 10:58:19 +0100

    Make GdsCard public and update version to 0.0.37

--- components/src/main/kotlin/se/seb/gds/preview/ListItemScreen.kt
+++ components/src/main/kotlin/se/seb/gds/preview/ListItemScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import se.seb.gds.atoms.cards.GdsCard
 import se.seb.gds.components.ListRowItem
 import se.seb.gds.icons.GdsIcons
 import se.seb.gds.theme.GdsTheme
@@ -45,7 +46,11 @@ fun ListItemScreen() {
             """.trimIndent(),
         )
 
-        CardColumn(modifier = Modifier.padding(16.dp)) {
+        GdsCard(
+            modifier = Modifier.padding(16.dp),
+            border = null,
+            containerColor = GdsTheme.colors.L2Neutral02,
+        ) {
             ListRowItem(
                 title = "Label 01",
             )
