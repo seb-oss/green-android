cef13053564133c5fce869578702b0e6bbd21c43
Lisandra Santos <lisandra.santos@seb.se>
2026-02-16 22:06:19 +0100

    Fix directory structure and remove duplicate screenshots

--- components/src/screenshotTest/kotlin/se/seb/gds/atoms/cards/InformationCardHdScreenshotTests.kt
+++ components/src/screenshotTest/kotlin/se/seb/gds/atoms/cards/InformationCardHdScreenshotTests.kt
@@ -1,4 +1,4 @@
-package atoms.cards
+package se.seb.gds.atoms.cards
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,45 +7,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.android.tools.screenshot.PreviewTest
-import se.seb.gds.atoms.cards.CardButton
-import se.seb.gds.atoms.cards.GdsCardDefaults
-import se.seb.gds.atoms.cards.GdsInformationCard
 import se.seb.gds.common.GdsUiPreview
 import se.seb.gds.theme.GdsTheme
-
-/**
- * Screenshot tests for the default information style of the [GdsInformationCard] component.
- */
-@PreviewTest
-@GdsUiPreview
-@Composable
-private fun GdsInfoCardScreenshot() {
-    GdsTheme {
-        Column(
-            modifier = Modifier.padding(horizontal = GdsTheme.dimensions.spacing.SpaceM),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            // Default Information Card with action button and dismiss icon
-            GdsInformationCard(
-                heading = "Information Card",
-                body = "This is an information card used for testing screenshot functionality.",
-                onDismiss = {},
-                button = CardButton(
-                    title = "Action",
-                    onClick = {},
-                ),
-            )
-
-            // Information Card without action button and dismiss icon
-            GdsInformationCard(
-                heading = "Information Card",
-                body = "This is an information card used for testing screenshot functionality.",
-                onDismiss = null,
-                button = null,
-            )
-        }
-    }
-}
 
 /**
  * Screenshot tests for the HD style of the [GdsInformationCard] component.
---
+++ components/src/screenshotTest/kotlin/se/seb/gds/atoms/cards/InformationCardScreenshotTests.kt
@@ -0,0 +1,45 @@
+package se.seb.gds.atoms.cards
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.android.tools.screenshot.PreviewTest
+import se.seb.gds.common.GdsUiPreview
+import se.seb.gds.theme.GdsTheme
+
+/**
+ * Screenshot tests for the default information style of the [GdsInformationCard] component.
+ */
+@PreviewTest
+@GdsUiPreview
+@Composable
+private fun GdsInfoCardScreenshot() {
+    GdsTheme {
+        Column(
+            modifier = Modifier.padding(horizontal = GdsTheme.dimensions.spacing.SpaceM),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            // Default Information Card with action button and dismiss icon
+            GdsInformationCard(
+                heading = "Information Card",
+                body = "This is an information card used for testing screenshot functionality.",
+                onDismiss = {},
+                button = CardButton(
+                    title = "Action",
+                    onClick = {},
+                ),
+            )
+
+            // Information Card without action button and dismiss icon
+            GdsInformationCard(
+                heading = "Information Card",
+                body = "This is an information card used for testing screenshot functionality.",
+                onDismiss = null,
+                button = null,
+            )
+        }
+    }
+}
